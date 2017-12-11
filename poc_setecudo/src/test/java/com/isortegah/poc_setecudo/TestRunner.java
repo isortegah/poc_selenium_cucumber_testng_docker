89fc2ed56b875b74c6c07340376b76e8c61cb3a4
isortegah <isortegah@gmail.com>
2017-12-10 21:20:20 -0600

    Dependencias para cucumber

---
+++ poc_setecudo/src/test/java/com/isortegah/poc_setecudo/TestRunner.java
@@ -0,0 +1,39 @@
+package com.isortegah.poc_setecudo;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ *
+ * @author isortegah
+ */
+public class TestRunner {
+    
+    public TestRunner() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
